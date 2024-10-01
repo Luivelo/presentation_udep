171cf5be77595e1cd1431105470028e15c79a124
Luivelo <111466442+Luivelo@users.noreply.github.com>
2024-10-01 13:21:13 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
 import streamlit as st
 import streamlit.components.v1 as components
 st.title('🎈 Presentation Deck')
-components.iframe("https://docs.google.com/presentation/d/e/2PACX-1vRvj0BkEqeymn2dp4ekSEeqD_l8HWvfzkWYo7w350eNjn6VcOja4HwRWcaONrs3pPnRukghtrFxG51c/embed?start=false&loop=false&delayms=3000", height=440)
+components.iframe("https://docs.google.com/presentation/d/e/2PACX-1vRvj0BkEqeymn2dp4ekSEeqD_l8HWvfzkWYo7w350eNjn6VcOja4HwRWcaONrs3pPnRukghtrFxG51c/embed?start=false&loop=false&delayms=3000", height=435)
